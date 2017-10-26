56b0537d72df8618eccb63c6d3e66d6e2049d780
p0keT <p0keT@ukr.net>
2017-10-26 23:45:18 +0300

    hyun9

--- src/main/java/GraphDrawing.java
+++ src/main/java/GraphDrawing.java
@@ -102,41 +102,95 @@ public class GraphDrawing extends GraphMatrix{
 
 
     ArrayList<Integer> criticalPaths = new ArrayList<Integer>();
+    int maxCriticalPathsVariable=0;
+    ArrayList<Integer> maxmimalPaths = new ArrayList<Integer>();
+    int maxPathsVariable=0;
+
+
+
 
     public void getCriticalPath(){
 
         for (int i = 1; i <= getNumberOfNodes() ; i++) {
             ArrayList<Integer> listOfNodePathes = new ArrayList<Integer>();
             listOfNodePathes.add(0);
+            ArrayList<Integer> listOfNodePathesV1 = new ArrayList<Integer>();
+            listOfNodePathesV1.add(0);
             //System.out.print(i+": ");
-            graphRecursion(i, listOfNodePathes);
+            graphRecursion(i, listOfNodePathes, listOfNodePathesV1);
             int max = listOfNodePathes.get(0);
             for (int j = 0; j <listOfNodePathes.size() ; j++) {
                 if(listOfNodePathes.get(j)>max)
                     max=listOfNodePathes.get(j);
             }
             criticalPaths.add(max);
+            int maxV1 = listOfNodePathesV1.get(0);
+            for (int j = 0; j <listOfNodePathesV1.size() ; j++) {
+                if(listOfNodePathesV1.get(j)>max)
+                    max=listOfNodePathesV1.get(j);
+            }
+            maxmimalPaths.add(maxV1);
             //System.out.println();
         }
         System.out.println(criticalPaths);
+        maxCriticalPathsVariable = criticalPaths.get(0);
+        for (int j = 0; j <criticalPaths.size() ; j++) {
+            if(criticalPaths.get(j)>maxCriticalPathsVariable)
+                maxCriticalPathsVariable=criticalPaths.get(j);
+        }
+        System.out.println("max: "+maxCriticalPathsVariable);
+        System.out.println(maxmimalPaths);
+        maxPathsVariable = maxmimalPaths.get(0);
+        for (int j = 0; j <maxmimalPaths.size() ; j++) {
+            if(maxmimalPaths.get(j)>maxPathsVariable)
+                maxPathsVariable=maxmimalPaths.get(j);
+        }
+        System.out.println("max: "+maxPathsVariable);
+
+        formulaV1();
         //sortingOutput();
     }
-    public void graphRecursion(int i, ArrayList<Integer> listOfNodePathes){
+
+    private void formulaV1(){
+        ArrayList<Double> results = new ArrayList<>();
+        for (int i = 0; i < getNumberOfNodes(); i++) {
+            results.add((maxmimalPaths.get(i)*1.0/maxPathsVariable)+(criticalPaths.get(i)*1.0/maxCriticalPathsVariable));
+            System.out.println("Result"+i+": " +results.get(results.size()-1));
+
+        }
+
+        HashMap<Integer,Double> mapToSort = new HashMap<Integer, Double>();
+        for (int i = 0; i <results.size() ; i++) {
+            mapToSort.put(i, results.get(i));
+        }
+        mapToSort = (HashMap<Integer, Double>) sortByValueDescending2(mapToSort);
+
+        System.out.println(mapToSort);
+    }
+
+    public void graphRecursion(int i, ArrayList<Integer> listOfNodePathes, ArrayList<Integer> listOfNodePathesV1){
         for (Edge e : graph.getNode(String.valueOf(i)).getEachLeavingEdge()) {
             int id = Integer.parseInt(e.getNode1().getId());
             //System.out.print(id + "/");
             listOfNodePathes.set(listOfNodePathes.size()-1,
                     listOfNodePathes.get(listOfNodePathes.size()-1)+super.getMatrix()[i-1][i-1]);
-            graphRecursion(id, listOfNodePathes);
+
+            listOfNodePathesV1.set(listOfNodePathesV1.size()-1,
+                    listOfNodePathesV1.get(listOfNodePathesV1.size()-1)+1);
+
+            graphRecursion(id, listOfNodePathes, listOfNodePathesV1);
 
         }
         if(graph.getNode(String.valueOf(i)).getLeavingEdgeSet().isEmpty()){
             listOfNodePathes.set(listOfNodePathes.size()-1,
                     listOfNodePathes.get(listOfNodePathes.size()-1)+super.getMatrix()[i-1][i-1]);
+            listOfNodePathesV1.set(listOfNodePathesV1.size()-1,
+                    listOfNodePathesV1.get(listOfNodePathesV1.size()-1)+1);
             listOfNodePathes.add(0);
+            listOfNodePathesV1.add(0);
             //System.out.println(listOfNodePathes);
         }
-        //System.out.print("+");
+//        System.out.print("+");
 
     }
 //
@@ -185,6 +239,24 @@ public class GraphDrawing extends GraphMatrix{
     }
 
     public static Map<Integer, Integer> sortByValueDescending(Map<Integer, Integer> map) {
+        List list = new LinkedList(map.entrySet());
+        Collections.sort(list, new Comparator() {
+
+            @Override
+            public int compare(Object o1, Object o2) {
+                return ((Comparable) ((Map.Entry) (o2)).getValue()).compareTo(((Map.Entry) (o1)).getValue());
+            }
+        });
+
+        Map result = new LinkedHashMap();
+        for (Iterator it = list.iterator(); it.hasNext();) {
+            Map.Entry entry = (Map.Entry) it.next();
+            result.put(entry.getKey(), entry.getValue());
+        }
+        return result;
+    }
+
+    public static Map<Integer, Double> sortByValueDescending2(Map<Integer, Double> map) {
         List list = new LinkedList(map.entrySet());
         Collections.sort(list, new Comparator() {
 
